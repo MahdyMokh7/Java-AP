f7bb8e03dfe6db572351dfbe96c0649b40095344
narges khansari <narges.1383asadikhansari@gmail.com>
2024-07-22 12:01:45 +0330

    class stage

---
+++ Stage.java
@@ -0,0 +1,12 @@
+public class Stage {  
+    private int id;
+    private int price;
+    private Time time;
+ 
+    public Stage(int id , int price, Time time) {  
+        
+        this.id = id;
+        this.price = price;  
+        this.time = time;  
+    }  
+}
